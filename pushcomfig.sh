7914181ac96e8e9136885163efd7550c806faefe
segfault <razor@localhost.(none)>
2009-12-02 12:23:02 +0500

    -a

--- pushcomfig.sh
+++ pushcomfig.sh
@@ -2,20 +2,20 @@
 # execute it for making symlinks in your workdirectory 
 # to files in ~/configs
 for ii in .*;do
-    if [ -e $ii ];then
-	if [ ! $ii = ".git" ]&&[ ! $ii = "." ]&&[ ! $ii = ".." ];then
-	    if [ -e ~/$ii ];then
-		if [ x$1 = "x-f" ];then
-		    rm -rf ~/$ii
-		    ln -s -T ~/configs/$ii ~/$ii
-		else
-		    echo "you can not do that::file ~/$ii is exist"
+	if [ -e $ii ];then
+		if [ ! $ii = ".git" ]&&[ ! $ii = "." ]&&[ ! $ii = ".." ];then
+			if [ -e ~/$ii ];then
+				if [ x$1 = "x-f" ];then
+					rm -rf ~/$ii
+					ln -s -T ~/configs/$ii ~/$ii
+				else
+					echo "you can not do that::file ~/$ii is exist"
+				fi
+			else
+				ln -s -T ~/configs/$ii ~/$ii
+			fi
 		fi
-	    else
-		ln -s -T ~/configs/$ii ~/$ii
-	    fi
 	fi
-    fi
 done
 ######################################################################
 # if you want use vimrc.local you must make symlink to ./vimrc.local #
