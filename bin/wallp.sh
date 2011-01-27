a812e383b1db47e98d995cd1d0ea492db4657d39
segfault <razor@calculate.local>
2011-01-27 12:14:50 +0000

    modified:   bin/wallp.sh 	два моника ура

--- bin/wallp.sh
+++ bin/wallp.sh
@@ -6,7 +6,11 @@
 [[ -f $1 ]] || exit 255
     
 while true;do
-    feh --bg-max "$(cat "$1" | shuf | head -n 1)"
+	 f1="$(cat "$1" | shuf | head -n 1)"
+	 f2="$(cat "$1" | shuf | head -n 1)"
+    nice -n 10 montage -tile 2x1 -geometry 1280x960 -background \#000000 "$f1" "$f2" tmp.png
+    nice -n 10 feh --bg-center tmp.png
+    rm tmp.png
     ss=$(seq $2 $3 | shuf | head -n1)
     sleep ${ss}s
 done
