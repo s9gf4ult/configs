c8b47d528dc3e961fe50285d3f310ec49ea297ac
s9gf4ult <s9gf4ult@gmail.com>
2012-11-04 19:44:36 +0600

    new file:   bin/update_gnucash.sh

---
+++ bin/update_gnucash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GNUCASHDIR="/home/razor/gnucash"
+SSHKEY="/home/razor/.ssh/bucket-s9gf4ult"
+
+ssh-agent bash <<EOF
+ssh-add $SSHKEY
+pushd $GNUCASHDIR
+git add .
+git commit -m "autocommit by script"
+git push origin master:master
+popd
+EOF
