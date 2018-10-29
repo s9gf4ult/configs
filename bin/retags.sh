b0e7ef2d96d63965773af29c55a11ed0ee94fc82
Aleksey Uimanov <s9gf4ult@gmail.com>
2018-10-30 01:12:23 +0500

    retags

---
+++ bin/retags.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+cd platform && hasktags -e --ignore-close-implementation $(find `pwd` -iname *.hs -and \( -not -path *.stack-work* \) -and \( -not -name Setup.hs \) -and -type f) && cd ../tinkoff-travel && hasktags -e --ignore-close-implementation $(find `pwd` -iname *.hs -and \( -not -path *.stack-work* \) -and \( -not -name Setup.hs \) -and -type f) && cd ../b2b && hasktags -e --ignore-close-implementation $(find `pwd` -iname *.hs -and \( -not -path *.stack-work* \) -and \( -not -name Setup.hs \) -and -type f) && cd ..
