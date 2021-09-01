42f147b6254f6cabe8136a2994ed43fd179a85c0
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-09-01 22:03:25 +0500

    monitor script

---
+++ bin/monitor.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+case $1 in
+  high)
+    ddccontrol -r 0x10 -w 100 dev:/dev/i2c-1
+    ddccontrol -r 0x10 -w 100 dev:/dev/i2c-2
+    ddccontrol -r 0x10 -w 70 dev:/dev/i2c-3
+    redshift -x
+    ;;
+  mid)
+    ddccontrol -r 0x10 -w 15 dev:/dev/i2c-1
+    ddccontrol -r 0x10 -w 15 dev:/dev/i2c-2
+    ddccontrol -r 0x10 -w 5 dev:/dev/i2c-3
+    redshift -x && redshift -O 3500
+    ;;
+  low)
+    ddccontrol -r 0x10 -w 0 dev:/dev/i2c-1
+    ddccontrol -r 0x10 -w 0 dev:/dev/i2c-2
+    ddccontrol -r 0x10 -w 0 dev:/dev/i2c-3
+    redshift -x && redshift -O 2500
+
+esac
