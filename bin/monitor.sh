cdffc81055cea577cc57be4074532bfe96f74f33
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-11-14 17:04:08 +0500

    fix

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -8,10 +8,10 @@ case $1 in
     redshift -x
     ;;
   mid)
-    ddccontrol -r 0x10 -w 15 dev:/dev/i2c-1
-    ddccontrol -r 0x10 -w 15 dev:/dev/i2c-2
-    ddccontrol -r 0x14 -w 11 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 5 dev:/dev/i2c-3
-    redshift -x && redshift -O 3500
+    ddccontrol -r 0x10 -w 23 dev:/dev/i2c-1
+    ddccontrol -r 0x10 -w 23 dev:/dev/i2c-2
+    ddccontrol -r 0x14 -w 11 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 8 dev:/dev/i2c-3
+    redshift -x && redshift -O 4000
     ;;
   night)
     ddccontrol -r 0x10 -w 7 dev:/dev/i2c-1
