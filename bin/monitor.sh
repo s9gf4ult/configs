545347e06c877b6acba3ac99865e56519da1555a
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-11-21 21:25:15 +0500

    fix

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -2,25 +2,25 @@
 
 case $1 in
   high)
-    ddccontrol -r 0x10 -w 100 dev:/dev/i2c-1
+    ddccontrol -r 0x14 -w 1 dev:/dev/i2c-1 && ddccontrol -r 0x10 -w 70 dev:/dev/i2c-1
     ddccontrol -r 0x10 -w 100 dev:/dev/i2c-2
     ddccontrol -r 0x14 -w 1 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 70 dev:/dev/i2c-3
     redshift -x
     ;;
   mid)
-    ddccontrol -r 0x10 -w 23 dev:/dev/i2c-1
+    ddccontrol -r 0x14 -w 11 dev:/dev/i2c-1 && ddccontrol -r 0x10 -w 14 dev:/dev/i2c-1
     ddccontrol -r 0x10 -w 23 dev:/dev/i2c-2
     ddccontrol -r 0x14 -w 11 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 8 dev:/dev/i2c-3
     redshift -x && redshift -O 4000
     ;;
   night)
-    ddccontrol -r 0x10 -w 7 dev:/dev/i2c-1
+    ddccontrol -r 0x14 -w 11 dev:/dev/i2c-1 && ddccontrol -r 0x10 -w 2 dev:/dev/i2c-1
     ddccontrol -r 0x10 -w 4 dev:/dev/i2c-2
     ddccontrol -r 0x14 -w 11 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 2 dev:/dev/i2c-3
     redshift -x && redshift -O 3000
     ;;
   low)
-    ddccontrol -r 0x10 -w 0 dev:/dev/i2c-1
+    ddccontrol -r 0x14 -w 11 dev:/dev/i2c-1 && ddccontrol -r 0x10 -w 0 dev:/dev/i2c-1
     ddccontrol -r 0x10 -w 0 dev:/dev/i2c-2
     ddccontrol -r 0x14 -w 11 dev:/dev/i2c-3 && ddccontrol -r 0x10 -w 0 dev:/dev/i2c-3
     redshift -x && redshift -O 2500
