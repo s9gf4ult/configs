8e471fec0d7482b012bbe6463cec79b4a20da43f
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-10-07 19:11:47 +0500

    fix for 2 mons

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-center="dev:/dev/i2c-6"
-left="dev:/dev/i2c-5"
-right="dev:/dev/i2c-7"
+center="dev:/dev/i2c-7"
+left="dev:/dev/i2c-6"
+# right="dev:/dev/i2c-7"
 
 case $1 in
   high)
     ddccontrol -r 0x14 -w 1 $center && ddccontrol -r 0x10 -w 70 $center
     ddccontrol -r 0x14 -w 1 $left && ddccontrol -r 0x10 -w 70 $left
-    ddccontrol -r 0x10 -w 100 $right
+    # ddccontrol -r 0x10 -w 100 $right
     redshift -x
     ;;
   mid)
-    ddccontrol -r 0x14 -w 11 $center && ddccontrol -r 0x10 -w 14 $center
-    ddccontrol -r 0x14 -w 11 $left && ddccontrol -r 0x10 -w 14 $left
-    ddccontrol -r 0x10 -w 23 $right
+    ddccontrol -r 0x14 -w 11 $center && ddccontrol -r 0x10 -w 18 $center
+    ddccontrol -r 0x14 -w 11 $left && ddccontrol -r 0x10 -w 18 $left
+    # ddccontrol -r 0x10 -w 23 $right
     redshift -x && redshift -O 4000
     ;;
   night)
     ddccontrol -r 0x14 -w 11 $center && ddccontrol -r 0x10 -w 2 $center
     ddccontrol -r 0x14 -w 11 $left && ddccontrol -r 0x10 -w 2 $left
-    ddccontrol -r 0x10 -w 4 $right
+    # ddccontrol -r 0x10 -w 4 $right
     redshift -x && redshift -O 3500
     ;;
   low)
     ddccontrol -r 0x14 -w 11 $center && ddccontrol -r 0x10 -w 0 $center
     ddccontrol -r 0x14 -w 11 $left && ddccontrol -r 0x10 -w 0 $left
-    ddccontrol -r 0x10 -w 0 $right
+    # ddccontrol -r 0x10 -w 0 $right
     redshift -x && redshift -O 2500
 
 esac
