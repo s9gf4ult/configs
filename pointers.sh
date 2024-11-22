5522ac2bc30556667b82c10411f27deed7f89ac5
Aleksey Uymanov <s9gf4ult@gmail.com>
2024-11-22 23:24:24 +0500

    fix

--- pointers.sh
+++ pointers.sh
@@ -3,26 +3,35 @@
 xset m 0
 xset r rate 200 100
 
-elecom='pointer:ELECOM TrackBall Mouse HUGE TrackBall'
-logitec='pointer:Logitech G403 Prodigy Gaming Mouse'
-kana='pointer:SteelSeries Kana v2 Gaming Mouse'
-zowie='pointer:Kingsis Corporation ZOWIE Gaming mouse'
+elecom=$(xinput list --id-only 'pointer:ELECOM TrackBall Mouse HUGE TrackBall' | grep -ve '^[[:space:]]*$')
 
-xinput set-prop "$elecom" 'libinput Scroll Method Enabled' 0, 0, 1
-xinput set-prop "$elecom" 'libinput Button Scrolling Button' 11
-xinput set-prop "$elecom" "libinput Accel Speed" -0.85
+if [ -n $elecom ]; then
+  xinput set-prop "$elecom" 'libinput Scroll Method Enabled' 0, 0, 1
+  xinput set-prop "$elecom" 'libinput Button Scrolling Button' 10
+  xinput set-prop "$elecom" "libinput Accel Speed" 0
+  xinput set-button-map "$elecom" 1 2 3 4 5 6 7 8 9 10 1 2
+fi
 
-xinput set-prop "$logitec" 'libinput Accel Profile Enabled' 0, 1
-xinput set-prop "$logitec" 'libinput Accel Speed' 0
-xinput set-prop "$logitec" 'libinput Scroll Method Enabled' 0, 0, 1
-xinput set-prop "$logitec" 'libinput Button Scrolling Button' 2
+logitec=$(xinput list --id-only 'pointer:Logitech G403 Prodigy Gaming Mouse')
+if [ -n $logitec ]; then
+  xinput set-prop "$logitec" 'libinput Accel Profile Enabled' 0, 1
+  xinput set-prop "$logitec" 'libinput Accel Speed' 0
+  xinput set-prop "$logitec" 'libinput Scroll Method Enabled' 0, 0, 1
+  xinput set-prop "$logitec" 'libinput Button Scrolling Button' 2
+fi
 
-xinput set-prop "$kana" 'libinput Accel Profile Enabled' 0, 1
-xinput set-prop "$kana" 'libinput Accel Speed' 0
-xinput set-prop "$kana" 'libinput Scroll Method Enabled' 0, 0, 1
-xinput set-prop "$kana" 'libinput Button Scrolling Button' 2
+kana=$(xinput list --id-only 'pointer:SteelSeries Kana v2 Gaming Mouse')
+if [ -n $kana ]; then
+  xinput set-prop "$kana" 'libinput Accel Profile Enabled' 0, 1
+  xinput set-prop "$kana" 'libinput Accel Speed' 0
+  xinput set-prop "$kana" 'libinput Scroll Method Enabled' 0, 0, 1
+  xinput set-prop "$kana" 'libinput Button Scrolling Button' 2
+fi
 
-xinput set-prop "$zowie" 'libinput Accel Profile Enabled' 0, 1
-xinput set-prop "$zowie" 'libinput Accel Speed' 0
-xinput set-prop "$zowie" 'libinput Scroll Method Enabled' 0, 0, 1
-xinput set-prop "$zowie" 'libinput Button Scrolling Button' 2
+zowie=$(xinput list --id-only 'pointer:Kingsis Corporation ZOWIE Gaming mouse')
+if [ -n $zowie ]; then
+  xinput set-prop "$zowie" 'libinput Accel Profile Enabled' 0, 1
+  xinput set-prop "$zowie" 'libinput Accel Speed' 0
+  xinput set-prop "$zowie" 'libinput Scroll Method Enabled' 0, 0, 1
+  xinput set-prop "$zowie" 'libinput Button Scrolling Button' 2
+fi
